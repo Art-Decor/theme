a0d2a1a4d133e5c705b26474692b1913919f6df9
Kamil <k.medrala99@gmail.com>
2023-07-10 09:22:53 +0200

    sticky header top style set fix, navbar collapse

--- components/we_megamenu/we_mobile_menu.js
+++ components/we_megamenu/we_mobile_menu.js
@@ -8,17 +8,24 @@
       accordionMenu: 'true',
       toggledClass: 'menu-active',
       pageSelector: 'body',
-      headerSelector: '[data-header-main]'
+      headerSelector: '[data-header-main]',
+      headerContainerSelector: '[data-header-container]'
     }, options);
 
-    let offsetTop = 0
-    if ($(settings.headerSelector).length) {
-      offsetTop = $(settings.headerSelector).offset().top + $(settings.headerSelector).height()
+    function _calcOffsetTop() {
+      if ($(window).width() > 991) {
+        return 0
+      }
+      let offsetTop = 0
+      if ($(settings.headerSelector).length) {
+        offsetTop = $(settings.headerSelector).offset().top + $(settings.headerSelector).height()
+      }
+      return offsetTop
     }
 
     if ($(window).width() <= 991) {
       $(settings.targetWrapper).addClass('mobile-main-menu');
-      $(settings.targetWrapper).css('top',`${offsetTop}px`)
+      $(settings.targetWrapper).css('top',`${_calcOffsetTop()}px`)
     }else{
       $(settings.targetWrapper).css('top','')
     }
@@ -29,7 +36,7 @@
     $(window).resize(function () {
       if ($(window).width() <= 991) {
         $(settings.targetWrapper).addClass('mobile-main-menu');
-        $(settings.targetWrapper).css('top',`${offsetTop}px`)
+        $(settings.targetWrapper).css('top',`${_calcOffsetTop()}px`)
       } else {
         $(settings.targetWrapper).removeClass('mobile-main-menu');
         $(settings.targetWrapper).css('top','')
@@ -71,9 +78,10 @@
       if (!wrapper.hasClass(settings.toggledClass)) {
         wrapper.addClass(settings.toggledClass).css('position', 'relative');
         $(settings.targetWrapper).addClass('mobile-main-menu');
+        $(settings.targetWrapper).css('top',`${_calcOffsetTop()}px`)
         targetWrapper.addClass('we-mobile-megamenu-active');
         if (wrapper.find('.overlay').length == 0) {
-          var overlay = $(`<div class="overlay" style="top: ${offsetTop}px"></div>`);
+          var overlay = $(`<div class="overlay" style="top: ${_calcOffsetTop()}px"></div>`);
           overlay.prependTo(wrapper);
           overlay.click(function () {
             _weMegaMenuClear();
@@ -122,7 +130,7 @@
               $sub_menu_inner.slideDown();
               setTimeout(function () {
                 $(targetWrapper).animate({
-                  scrollTop: $this.offset().top + offsetTop
+                  scrollTop: $this.offset().top + _calcOffsetTop()
                 }, 700);
               }, 500);
             }
@@ -133,6 +141,22 @@
         }
       });
     }
+
+
+    let scrollState = 0 
+
+    window.addEventListener('scroll',()=>{
+      
+      if (window.scrollY < 100) {
+        $(settings.headerContainerSelector).removeClass('nav-collapsed')
+      }else if(scrollState > window.scrollY) {
+        $(settings.headerContainerSelector).removeClass('nav-collapsed')        
+      }else{
+        $(settings.headerContainerSelector).addClass('nav-collapsed')
+      }
+
+      scrollState = window.scrollY
+    })
   }
 
 }(jQuery));
