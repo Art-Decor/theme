4726097166276399eff25c9e5d128485b7e1549f
Kamil <k.medrala99@gmail.com>
2023-08-16 11:12:21 +0200

    adding js fix

--- assets/custom.js
+++ assets/custom.js
@@ -1,6 +1,6 @@
 Drupal.behaviors.overrideSwiper = {
 attach: function(context, settings) {
-    var swiperSettings = drupalSettings.swiper_formatter_settings && drupalSettings.swiper_formatter_settings.swipers['swiper-slider-produktow-block-1'] ? drupalSettings.swiper_formatter_settings.swipers['swiper-featured-watches-block-1'] : null;
+    var swiperSettings = drupalSettings.swiper_formatter_settings && drupalSettings.swiper_formatter_settings.swipers['swiper-slider-produktow-block-1--5'] ? drupalSettings.swiper_formatter_settings.swipers['swiper-slider-produktow-block-1--5'] : null;
     
     if (swiperSettings) {
     swiperSettings.slidesPerView = 1.5
