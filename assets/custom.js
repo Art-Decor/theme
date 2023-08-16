40810baea0195752e85a1332f8c5b2dc371b83e2
Kamil <k.medrala99@gmail.com>
2023-08-16 10:49:22 +0200

    adding js fix

--- assets/custom.js
+++ assets/custom.js
@@ -1,23 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const swiperEl = document.querySelector('.view-id-slider_produktow .swiper-container');
-    if(swiperEl){
-      swiperEl.swiper.params.slidesPerView = 1.5
-      swiperEl.swiper.params.breakpoints['540'] = {slidesPerView: 2.5}
-      swiperEl.swiper.update()
+Drupal.behaviors.overrideSwiper = {
+attach: function(context, settings) {
+    var swiperSettings = drupalSettings.swiper_formatter_settings && drupalSettings.swiper_formatter_settings.swipers['swiper-slider-produktow-block-1'] ? drupalSettings.swiper_formatter_settings.swipers['swiper-featured-watches-block-1'] : null;
+    
+    if (swiperSettings) {
+    swiperSettings.slidesPerView = 1.5
+    swiperSettings.breakpoints = {
+        540: {
+        slidesPerView: 2.5,
+        },
+    };
     }
-  });
-
-  Drupal.behaviors.overrideSwiper = {
-    attach: function(context, settings) {
-      var swiperSettings = drupalSettings.swiper_formatter_settings && drupalSettings.swiper_formatter_settings.swipers['swiper-slider-produktow-block-1'] ? drupalSettings.swiper_formatter_settings.swipers['swiper-featured-watches-block-1'] : null;
-      
-      if (swiperSettings) {
-        swiperSettings.slidesPerView = 1.5
-        swiperSettings.breakpoints = {
-          540: {
-            slidesPerView: 2.5,
-          },
-        };
-      }
-    }
-  };
+}
+};
