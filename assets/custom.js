addf22b6b4bd9ff357451c202e590d5351e3a314
Kamil <k.medrala99@gmail.com>
2023-08-16 14:33:44 +0200

    custom swiper settings

--- assets/custom.js
+++ assets/custom.js
@@ -3,14 +3,15 @@ attach: function(context, settings) {
     var swipers = drupalSettings.swiper_formatter && drupalSettings.swiper_formatter.swipers ? drupalSettings.swiper_formatter.swipers : null;
     
     if (swipers) {
-        swipers.forEach((swiper)=>{
+        for (const key in swipers) {
+            const swiper = swipers[key];                
             if (swiper.label =- 'Products swiper custom') {
                 swiperSettings.slidesPerView = 1.5
                 swiperSettings.breakpoints['500'] = {
                     slidesPerView: 2.5,
                 };
             }
-        })
+        }
     }
 }
 };
