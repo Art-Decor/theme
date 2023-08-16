c24b0680f51551fb0ea7e9e43db2943aaa96645a
Kamil <k.medrala99@gmail.com>
2023-08-16 13:49:27 +0200

    custom swiper settings fix

--- assets/custom.js
+++ assets/custom.js
@@ -1,6 +1,6 @@
 Drupal.behaviors.overrideSwiper = {
 attach: function(context, settings) {
-    var swiperSettings = drupalSettings.swiper_formatter_settings && drupalSettings.swiper_formatter_settings.swipers['swiper-slider-produktow-block-1--5'] ? drupalSettings.swiper_formatter_settings.swipers['swiper-slider-produktow-block-1--5'] : null;
+    var swiperSettings = drupalSettings.swiper_formatter && drupalSettings.swiper_formatter.swipers['swiper-slider-produktow-block-1--5'] ? drupalSettings.swiper_formatter.swipers['swiper-slider-produktow-block-1--5'] : null;
     
     if (swiperSettings) {
     swiperSettings.slidesPerView = 1.5
