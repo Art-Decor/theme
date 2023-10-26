3984ff926660cfcafb4df1bba8b278a620a924b7
Kamil <k.medrala99@gmail.com>
2023-10-26 14:05:05 +0200

    code cleanup, order fix

--- assets/product_options.js
+++ assets/product_options.js
@@ -1,41 +1,34 @@
-// window.addEventListener('DOMContentLoaded',() => {
-//     syncOptionsHandler()
-//     if (jQuery) {
-//         jQuery(document).on('ajaxComplete',syncOptionsHandler)
-//     }
-// });
-
-
-// function syncOptionsHandler() {    
-//     const displayedInputs = document.querySelectorAll('[data-drupal-selector="edit-purchased-entity-wrapper"] input')
-//     const optionsWrapper = document.querySelector('[data-drupal-selector="edit-field-options-wrapper"]')
-//     const optionsInputs = document.querySelectorAll('[data-drupal-selector="edit-field-options-wrapper"] input')    
-
-//     if (optionsWrapper) {
-//         optionsWrapper.classList.add('!hidden');
-//     }
-
-//     if (optionsInputs) {
-//         clearCheckboxes(optionsInputs)
-//     }
-
-//     if (displayedInputs && optionsInputs) {
-//         displayedInputs.forEach(displayedInput=>{
-//             if (displayedInput.checked) {
-//                 clearCheckboxes(optionsInputs)
-//                 setCheckByValue(displayedInput.value,optionsInputs)
-//             }
-//         })
-//     }
-// }
-
-// function clearCheckboxes(inputs){
-//     inputs.forEach(element => {
-//         element.checked = false
-//     });
-// }
+const typeAttrsMap = {
+    117: ['Size','Canvas Wrap','Canvas Type','Floating Frame'],
+    118: ['Size','Frame','Fine Art Print Types','Mat','Mat Width'],
+    119: ['Fine Art Print Types','Size','Mat','Mat Width','Frame'],
+    120: ['Size','Thickness','Hanging System'],
+    121: ['Metal Print Options','Size','Corner Options','Hanging System'],
+    122: ['Wood Printing Options','Size','Hanging System']
+}
 
 optionsListenerInit()
+
+window.addEventListener('DOMContentLoaded',()=>{
+    (function($) {
+        Drupal.behaviors.addToCart = {
+          attach: function(context, settings) {
+            $('.commerce-order-item-add-to-cart-form', context).on('submit', function () {
+                const checkboxInputs = document.querySelectorAll('[data-options-checkboxes] input')
+                const radioInputs = document.querySelectorAll('input[data-input-group]')
+                if (checkboxInputs && radioInputs) {
+                    radioInputs.forEach(el => {
+                        if (el.checked) {
+                            setCheckByValue(el.value,checkboxInputs)
+                        }
+                    })
+                }
+                debugger;
+            });
+          }
+        }
+      })(jQuery);
+})
 
 function optionsListenerInit(){
     document.addEventListener('change',(event)=>{
@@ -48,15 +41,6 @@ function optionsListenerInit(){
         setDisplayedVal(input)
 
     })
-}
-
-const typeAttrsMap = {
-    117: ['Size','Canvas Wrap','Canvas Type','Floating Frame'],
-    118: ['Size','Frame','Fine Art Print Types','Mat','Mat Width'],
-    119: ['Fine Art Print Types','Size','Mat','Mat Width','Frame'],
-    120: ['Size','Thickness','Hanging System'],
-    121: ['Metal Print Options','Size','Corner Options','Hanging System'],
-    122: ['Wood Printing Options','Size','Hanging System']
 }
 
 function toggleVisibleAttributes(typeId,attrElements){
@@ -96,45 +80,6 @@ function setDisplayedVal(input) {
         displayField.textContent = valueLabel
     }    
 }
-
-// function clearCheckGroup(inputGroup) {
-//     const radioInputs = document.querySelectorAll(`input[data-input-group="${inputGroup}"]`)
-//     radioInputs.forEach(input => setCheckByValue(input.value,false))
-// }
-
-// function setCheckByValue(externalValue, value = true){
-//     const checkboxInputs = document.querySelectorAll('[data-options-checkboxes] input')
-
-//     if (!checkboxInputs) return
-
-//     const foundInput = Array.from(checkboxInputs).find(el => el.value == externalValue)
-
-//     if (!foundInput) return
-
-//     foundInput.checked = value
-// }
-
-
-window.addEventListener('DOMContentLoaded',()=>{
-    (function($) {
-        Drupal.behaviors.addToCart = {
-          attach: function(context, settings) {
-            $('.commerce-order-item-add-to-cart-form', context).on('submit', function () {
-                const checkboxInputs = document.querySelectorAll('[data-options-checkboxes] input')
-                const radioInputs = document.querySelectorAll('input[data-input-group]')
-                if (checkboxInputs && radioInputs) {
-                    radioInputs.forEach(el => {
-                        if (el.checked) {
-                            setCheckByValue(el.value,checkboxInputs)
-                        }
-                    })
-                }
-                debugger;
-            });
-          }
-        }
-      })(jQuery);
-})
 
 function setCheckByValue(externalVal,checkboxInputs){
     const foundCheckbox = Array.from(checkboxInputs).find(checkboxInput => checkboxInput.value == externalVal )
